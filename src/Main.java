55b65a4f83b0b6163ff0cd50d15495a2d44d81fb
divyajawahar <divyaj3010@gmail.com>
2025-08-15 23:17:59 +1000

    Added a simple function to greet our player's

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("He;l!");
-        System.out.println("help");
+        System.out.println(sayHi());
+    }
+
+    static String sayHi() {
+        return "Hello World";
     }
 }
