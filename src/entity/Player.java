1743f33adcc340e7a80dbad167942aa5dbdc161c
kayercws <kaylacwongso@gmail.com>
2025-08-16 17:36:25 +1000

    hello

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -13,18 +13,23 @@ public class Player extends Entity{
 
   GamePanel gamePanel;
   KeyHandler keyH;
+  public final int screenX;
+  public final int screenY;
 
   public Player(GamePanel gamePanel, KeyHandler keyH) {
       this.gamePanel = gamePanel;
       this.keyH = keyH;
+
+      screenX = gamePanel.screenWidth/2;
+      screenY = gamePanel.screenHeight/2;
 
       setDefaultValues();
       getPlayerImage();
   }
 
   public void setDefaultValues() {
-      x = 100;
-      y = 100;
+      worldX = gamePanel.tileSize * 23;
+      worldY = gamePanel.tileSize * 21;
       speed = 4;
       direction = "down";
   }
@@ -32,11 +37,10 @@ public class Player extends Entity{
   public void getPlayerImage(){
       try{
 
-          front = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty01 FRONT.png"));
-          back = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty01 BACK.png"));
-          down = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty01Down.png"));
-          left = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty01LEFT.png"));
-          right = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty01RIGHT.png"));
+          front = ImageIO.read(getClass().getResourceAsStream("/kitty/kitty02FRONT2.png"));
+          back = ImageIO.read(getClass().getResourceAsStream("/kitty/kitty02BACK.png"));
+          left = ImageIO.read(getClass().getResourceAsStream("/kitty/kitty02LEFTy.png"));
+          right = ImageIO.read(getClass().getResourceAsStream("/kitty/Kitty02RIGHT.png"));
 
 
 
@@ -50,19 +54,19 @@ public class Player extends Entity{
   public void update() {
       if(keyH.upPressed == true){
           direction = "up";
-          y -= speed;
+          worldY -= speed;
       }
       else if (keyH.downPressed == true) {
           direction = "down";
-          y += speed;
+          worldY += speed;
       }
       else if (keyH.leftPressed == true){
           direction = "left";
-          x -= speed;
+          worldX -= speed;
       }
       else if (keyH.rightPressed == true) {
           direction = "right";
-          x += speed;
+          worldX += speed;
       }
   }
 
@@ -88,7 +92,7 @@ public class Player extends Entity{
       }
       int w = image.getWidth();
       int h = image.getHeight();
-      g2.drawImage(image, x, y, w * 5, h * 5, null); // scales up 3x without stretching
+      g2.drawImage(image, screenX, screenY, w * 3, h * 3, null); // scales up 3x without stretching
 
   }
 }
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -85,13 +85,18 @@ public class TileManager {
             int worldX = worldCol * gp.tileSize;
             int worldY = worldRow * gp.tileSize;
             int screenX = worldX - gp.player.worldX + gp.player.screenX;
-            int screenY = worldY - gp.player.worldY * gp.player.screenY;
+            int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
+
+            // ASK CHAT
+            if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+            worldY + gp.tileSize > gp.player.worldY - gp.player.screenY && worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+                g2.drawImage(tile[tileNum].image, screenX, screenY,gp.tileSize, gp.tileSize, null);
+            } // this creates a boundary
             // map is stored here
-            g2.drawImage(tile[tileNum].image, screenX, screenY,gp.tileSize, gp.tileSize, null);
             worldCol++;
 
-            if(worldCol == gp.maxScreenCol) {
+            if(worldCol == gp.maxWorldCol) {
                 worldCol = 0;
                 worldRow++;
             }
