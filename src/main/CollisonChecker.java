1583b65f63736af0a61e1ae6324432c5f536c513
divyajawahar <divyaj3010@gmail.com>
2025-08-16 18:37:34 +1000

    added kitty moving, and added dirt, tree and sand

---
+++ src/main/CollisonChecker.java
@@ -0,0 +1,17 @@
+package main;
+
+import entity.Entity;
+
+import java.util.Map;
+
+public class CollisonChecker {
+    GamePanel gamePanel;
+
+    public CollisonChecker(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+    }
+
+    public void checkTile(Entity entity){
+
+    }
+}
